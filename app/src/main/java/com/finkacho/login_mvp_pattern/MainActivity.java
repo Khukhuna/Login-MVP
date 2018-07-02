ebbbee8d3738a632154584f4ec2489b8159ea47a
FiNkacho <sabaxuxunashvili@gmail.com>
2018-07-02 15:39:41 +0400

    Initial commit

---
+++ app/src/main/java/com/finkacho/login_mvp_pattern/MainActivity.java
@@ -0,0 +1,13 @@
+package com.finkacho.login_mvp_pattern;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
